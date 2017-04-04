c2cb51b0d0713d98fb73afc7a423309557d9234d
hypn0t0ad <blackfor3st@gmail.com>
2017-04-04 21:19:34 +0100

    hej hej

---
+++ Zensify/Zensify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zensify.Startup))]
+namespace Zensify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
